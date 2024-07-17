a14b180fba660da110618dde7837682c72c5d15e
GaRIEN <gabrielhuaman683@gmail.com>
2024-07-17 12:01:00 -0500

    configuracion de vistas en panel de administrador

--- Sicotec/proyectos/admin.py
+++ Sicotec/proyectos/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-
-from .models import Proyecto
-
-
-admin.site.register(Proyecto)
+from .models import Proyecto, Pais, Tipo_Proyecto, Entidad_Financiamiento, Institucion_Financiamiento, Tipo_Apoyo, Area_Tematica, Tipo_Moneda
 
 # Register your models here.
+admin.site.register(Proyecto)
+admin.site.register(Pais)
+admin.site.register(Tipo_Proyecto)
+admin.site.register(Entidad_Financiamiento)
+admin.site.register(Institucion_Financiamiento)
+admin.site.register(Tipo_Apoyo)
+admin.site.register(Area_Tematica)
+admin.site.register(Tipo_Moneda)
