f6733b6ea900932d29320328adb4e1a8aabea1eb
GaRIEN <gabrielhuaman683@gmail.com>
2024-07-28 12:37:16 -0500

    cambios en la tabla proyectos

--- Sicotec/core/static/core/js/datatable-init/datatable-proyectos-totales.js
+++ Sicotec/core/static/core/js/datatable-init/datatable-proyectos-totales.js
@@ -3,28 +3,16 @@ $(document).ready(function () {
       if (index < $("#tblProyectos thead th").length - 1) {
         var title = $("#tblProyectos thead th").eq(index).text();
         var inputHTML = '<input  type="text" placeholder="' + title + '"';
-        if (index === 0 || index === 1 || index === 4) {
+        if (index === 0 ||  index === 4 || index === 5) {
           inputHTML += ' style="max-width: 134px; text-align: center;"';
         }
-        if (index === 2 || index === 6 || index === 7) {
+        if (index === 1 ) {
           inputHTML += ' style="width: 300px !important; text-align: center;"';
         }
-        if (index === 3) {
-          inputHTML += ' style="width: 450px !important; text-align: center;"';
+        if (index === 2 ||  index === 3) {
+          inputHTML += ' style= "text-align: center;"';
         }
-        if (index === 8) {
-          inputHTML += ' style="width: 160px !important; text-align: center;"';
-        }
-        if (index === 11 || index === 12) {
-          inputHTML += ' style="max-width: 250px; text-align: center;"';
-        }
-        if (index === 14 || index === 15) {
-          inputHTML += ' style="max-width: 100px; text-align: center;"';
-        }
-        if (index === 5 || index === 9 || index === 10 || index === 13) {
-          inputHTML += ' style="text-align: center;"';
-        }
-  
+
         inputHTML += " />";
         $(this).html(inputHTML);
       }
