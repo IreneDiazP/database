fe41f85642a5de5f3c992a9d2ae3d5e7b40dce2e
GaRIEN <gabrielhuaman68364gmail.com>
2024-07-29 17:54:26 -0500

    crearion de urls para editar participante

--- Sicotec/core/static/core/js/participantes/todoslosparticipantes.js
+++ Sicotec/core/static/core/js/participantes/todoslosparticipantes.js
@@ -1,14 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tablaparticipante = document.getElementById('tblParticipantes');
+document.addEventListener("DOMContentLoaded", function () {
+    const tablaparticipante = document.getElementById("tblParticipantes");
 
-    tablaparticipante.addEventListener('click', function(event) {
-        console.log('click');
+    tablaparticipante.addEventListener("click", function (event) {
+        console.log("click");
         const target = event.target;
-        if (target.tagName === 'TH') {
+
+        if (target.tagName === "TH") {
             let row = target.parentNode;
-            let cells = row.getElementsByTagName('th'); // Corregido aquí
-            var cellData = Array.from(cells).map(cell => cell.textContent);
-            window.location.href = '../editarparticipante/';
+
+            if (row.tagName === "TR") {
+                let rowId = row.id;
+                console.log("ID de la fila:", rowId);
+
+              
+                // window.location.href = `/editarparticipante/?id=${rowId}`;
+            }
         }
     });
 });
