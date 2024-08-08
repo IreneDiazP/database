49dc3732125d84b76df8e24c63ca00613fd57919
GaRIEN <gabrielhuaman68364gmail.com>
2024-08-07 21:25:15 -0500

    cambio en tipo de moneda

--- Sicotec/core/static/core/js/inicializador/inicializador.js
+++ Sicotec/core/static/core/js/inicializador/inicializador.js
@@ -1,65 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document
-      .getElementById("cboTipoEnFinanciamiento")
-      .addEventListener("change", async function () {
-        var entidadID = this.value;
-        if (entidadID) {
-          try {
-            const response = await fetch(
-              `/proyecto/getInstituciones/${entidadID}/`,
-              {
-                method: "GET",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-  
-            if (!response.ok) {
-              throw new Error(`La respuesta de la red no fue correcta: ${response.statusText}`);
+  document
+    .getElementById("cboTipoEnFinanciamiento")
+    .addEventListener("change", async function () {
+      var entidadID = this.value;
+      if (entidadID) {
+        try {
+          const response = await fetch(
+            `/proyecto/getInstituciones/${entidadID}/`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
             }
-  
-            const data = await response.json();
-            let cboInstituciones = document.getElementById(
-              "cboTipoInsFinanciamiento"
-            );
-            cboInstituciones.innerHTML = "<option selected></option>";
-            data.forEach(function (item) {
-              let option = document.createElement("option");
-              option.value = item.id;
-              option.textContent = item.cInstFinancia;
-              cboInstituciones.appendChild(option);
-            });
-          } catch (error) {
-            console.error(
-              "Hubo un problema con la operación de búsqueda:",
-              error
+          );
+
+          if (!response.ok) {
+            throw new Error(
+              `La respuesta de la red no fue correcta: ${response.statusText}`
             );
           }
-        } else {
-          document.getElementById("cboTipoInsFinanciamiento").innerHTML = "";
-        }
-      });
-  });
-  
 
-  //EVENTO PARA CALCULAR TOTAL
-document.getElementById("txtMonto").addEventListener("input", function () {
-    let monto = this.value;
-    console.log("el monto es :" + monto);
-    let tipocambio = document.getElementById("txttipoCambio").value;
-  
-    total = monto * tipocambio;
-    console.log("el total es " + total);
-    document.getElementById("txtMontosoles").value = total;
-  });
-  
-  document.getElementById("txttipoCambio").addEventListener("input", function () {
-    let tipocambio = this.value;
-    let monto = document.getElementById("txtMonto").value;
-  
-    total = monto * tipocambio;
-    console.log("el total es " + total);
-    document.getElementById("txtMontosoles").value = total;
-  });
-  
+          const data = await response.json();
+          let cboInstituciones = document.getElementById(
+            "cboTipoInsFinanciamiento"
+          );
+          cboInstituciones.innerHTML = "<option selected></option>";
+          data.forEach(function (item) {
+            let option = document.createElement("option");
+            option.value = item.id;
+            option.textContent = item.cInstFinancia;
+            cboInstituciones.appendChild(option);
+          });
+        } catch (error) {
+          console.error(
+            "Hubo un problema con la operación de búsqueda:",
+            error
+          );
+        }
+      } else {
+        document.getElementById("cboTipoInsFinanciamiento").innerHTML = "";
+      }
+    });
+});
+
+
+function clickmonto(monto) {
+  console.log("el monto es :" + monto);
+  let tipocambio = document.getElementById("txttipoCambio").value;
+  total = monto * tipocambio;
+  console.log("el total es " + total);
+  document.getElementById("txtMontosoles").value = total;
+}
+
+function clicktipocammbio(tipocambio) {
+  let monto = document.getElementById("txtMonto").value;
+
+  total = monto * tipocambio;
+  console.log("el total es " + total);
+  document.getElementById("txtMontosoles").value = total;
+}
+
+function VerificarTipoMoneda(TipoMoneda) {
+  const tipo = TipoMoneda.toLowerCase();
+  const esSoles = tipo === "soles";
+
+  const tipoCambioInput = document.getElementById("txttipoCambio");
+  const montoInput = document.getElementById("txtMonto");
+  const montosolesInput = document.getElementById("txtMontosoles");
+
+  tipoCambioInput.disabled = esSoles;//true o false
+  tipoCambioInput.value = esSoles ? "1" : "";
+  montoInput.value = "";
+  montosolesInput.value = "";
+}
