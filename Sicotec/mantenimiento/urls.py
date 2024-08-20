4e684e3d9f6276a96fb09b01c5e18a5f2d1f785c
GaRIEN <gabrielhuaman68364gmail.com>
2024-08-20 00:56:45 -0500

    avance crud entidades

--- Sicotec/mantenimiento/urls.py
+++ Sicotec/mantenimiento/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    #CRUD INSTITUCIONES
     path('añadirnuevo/',views.nuevaInstitucion, name='nuevaInstitucion'),
-    # traer datos de la institucion
     path('getInstitucion/<int:idinstitucion>/',views.getInstitucion, name='getInstitucion'),
     
     path('editarinstitucion/',views.editarinstitucion, name='editarinstitucion'),
     
-    path('eliminarInstituciones/',views.eliminarInstituciones, name='eliminarInstituciones')
+    path('eliminarInstituciones/',views.eliminarInstituciones, name='eliminarInstituciones'),
+    
+    # CRUD ENTIDADES
+    path('añadirnuevoentidad/',views.añadirnuevoentidad, name='añadirnuevoentidad'),
+        
+    path('getEntidad/<int:identidad>/',views.getEntidad, name='getEntidad'),
 ]
--- Sicotec/mantenimiento/views.py
+++ Sicotec/mantenimiento/views.py
@@ -1,30 +1,38 @@
 import json
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from proyectos.models import Entidad_Financiamiento,Institucion_Financiamiento
+from proyectos.models import Entidad_Financiamiento, Institucion_Financiamiento
 
 # Create your views here.
-def nuevaInstitucion (request):
-    Entidades=Entidad_Financiamiento.objects.all().order_by('cEntFinancia')
-    
-    instituciones=Institucion_Financiamiento.objects.all().order_by('cInstFinancia')
 
-    return render(request,'mantenimiento/nuevainstitucion.html',{
-        'Entidades':Entidades,
-        'instituciones':instituciones
+
+@login_required
+def nuevaInstitucion(request):
+    Entidades = Entidad_Financiamiento.objects.all().order_by('cEntFinancia')
+
+    instituciones = Institucion_Financiamiento.objects.all().order_by('cInstFinancia')
+
+    return render(request, 'mantenimiento/nuevainstitucion.html', {
+        'Entidades': Entidades,
+        'instituciones': instituciones
     })
 
-def getInstitucion(request,idinstitucion):
+
+@login_required
+def getInstitucion(request, idinstitucion):
     try:
-        institucion = Institucion_Financiamiento.objects.get(id = idinstitucion)
-        datainstitucion={
-            'nombreInstitucion':institucion.cInstFinancia,
-            'identidad':institucion.entidad_financiamiento.id
+        institucion = Institucion_Financiamiento.objects.get(id=idinstitucion)
+        datainstitucion = {
+            'nombreInstitucion': institucion.cInstFinancia,
+            'identidad': institucion.entidad_financiamiento.id
         }
-        return JsonResponse({'success':True, 'data':datainstitucion })
+        return JsonResponse({'success': True, 'data': datainstitucion})
     except Exception as e:
         return JsonResponse({'success': False, 'message': str(e)})
 
+
+@login_required
 def editarinstitucion(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -44,15 +52,16 @@ def editarinstitucion(request):
                 nuevaInstitucion.save()
                 message = 'La institución fue registrada exitosamente'
             else:
-                getinstitucion = Institucion_Financiamiento.objects.get(id=idInstitucion)
+                getinstitucion = Institucion_Financiamiento.objects.get(
+                    id=idInstitucion)
                 getinstitucion.cInstFinancia = institucion
                 getinstitucion.entidad_financiamiento = Entidad_instance
                 getinstitucion.lestado = True
                 getinstitucion.save()
                 message = 'La institución fue modificada exitosamente'
 
-
-            instituciones = Institucion_Financiamiento.objects.all().select_related('entidad_financiamiento')
+            instituciones = Institucion_Financiamiento.objects.all(
+            ).select_related('entidad_financiamiento')
 
             instituciones_list = [
                 {
@@ -66,15 +75,40 @@ def editarinstitucion(request):
 
         except Exception as e:
             return JsonResponse({'success': False, 'message': f'Error en los datos recibidos: {str(e)}'}, status=400)
-        
-def eliminarInstituciones (request):
+
+
+@login_required
+def eliminarInstituciones(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            idinstitucion=data.get('idinstitucion')
-            institucion = Institucion_Financiamiento.objects.get(id = idinstitucion)
+            idinstitucion = data.get('idinstitucion')
+            institucion = Institucion_Financiamiento.objects.get(
+                id=idinstitucion)
             institucion.delete()
-            
-            return JsonResponse({'success':True, 'message': 'Institucion eliminado correctamente'})
+
+            return JsonResponse({'success': True, 'message': 'Institucion eliminado correctamente'})
         except Exception as e:
-             return JsonResponse({'success': False, 'message': f'Error en los datos recibidos: {str(e)}'}, status=400)
+            return JsonResponse({'success': False, 'message': f'Error en los datos recibidos: {str(e)}'}, status=400)
+
+
+@login_required
+def añadirnuevoentidad(request):
+    Entidades = Entidad_Financiamiento.objects.all().order_by('cEntFinancia')
+
+    return render(request, 'mantenimiento/nuevoEntidad.html', {
+        'Entidades': Entidades,
+    })
+
+@login_required
+def getEntidad(request, identidad):
+    try:
+        print('holi boli')
+        Entidad = Entidad_Financiamiento.objects.get(id=identidad)
+        datainstitucion = {
+            'nombreEntidad': Entidad.cEntFinancia,
+           
+        }
+        return JsonResponse({'success': True, 'data': datainstitucion})
+    except Exception as e:
+        return JsonResponse({'success': False, 'message': str(e)})
