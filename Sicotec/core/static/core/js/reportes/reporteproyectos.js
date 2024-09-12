89381586636b0484753156106b2d0f6e495bad95
GaRIEN <gabrielhuaman68364gmail.com>
2024-09-12 11:13:36 -0500

    correcion de observaciones en reportes de proyectos

--- Sicotec/core/static/core/js/reportes/reporteproyectos.js
+++ Sicotec/core/static/core/js/reportes/reporteproyectos.js
@@ -40,20 +40,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document
-  .getElementById("button_Exportar")
-  .addEventListener("click", function () {
-    const element = document.getElementById("vistaPreviaReporte");
+// document
+//   .getElementById("button_Exportar")
+//   .addEventListener("click", function () {
+//     const element = document.getElementById("vistaPreviaReporte");
 
-    const options = {
-      margin: [10, 15, 10, 10],
-      filename: "reporte_area.pdf",
-      html2canvas: { scale: 2, useCORS: true },
-      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-    };
+//     const options = {
+//       margin: [10, 15, 10, 10],
+//       filename: "reporte_area.pdf",
+//       html2canvas: { scale: 2, useCORS: true },
+//       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+//     };
 
+//     html2pdf().from(element).set(options).save();
+//   });
+
+document.getElementById("button_Exportar").addEventListener("click", function () {
+  const element = document.getElementById("vistaPreviaReporte");
+
+  const options = {
+    margin: [10, 15, 10, 10],
+    filename: "reporte_area.pdf",
+    html2canvas: { scale: 2, useCORS: true },
+    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    pagebreak: { mode: 'avoid-all' }
+  };
+
+  html2pdf().from(element).toPdf().get('pdf').then(function (pdf) {
+    // Total number of pages
+    const totalPages = pdf.internal.getNumberOfPages();
+    
+    // Add page count to the content
+    const pageCountText = `Página ${pdf.internal.getCurrentPageInfo().pageNumber} de ${totalPages}`;
+    document.getElementById("page-count").textContent = pageCountText;
+
+    // Re-generate the PDF to include the page count
     html2pdf().from(element).set(options).save();
   });
+});
 
 document
   .getElementById("button_NuevoReporte")
--- Sicotec/reportes/views.py
+++ Sicotec/reportes/views.py
@@ -5,12 +5,13 @@ from becarios.models import Participante
 from django.db import connection
 from collections import defaultdict
 from proyectos.models import Area_Tematica
+from datetime import datetime
 
 
 def reporteproyecto(request):
     areas = Area_Tematica.objects.all().order_by('cArea_tematica')
-    return render(request,'reportes/reporteproyectos.html',{
-        'areas':areas
+    return render(request, 'reportes/reporteproyectos.html', {
+        'areas': areas
     })
 
 
@@ -18,9 +19,9 @@ def generar_reporte_area(request):
     if request.method == 'POST':
         año = request.POST.get('Año')
         area_tematica = request.POST.get('cboAreaTematica')
-        
-        if  area_tematica :
-            nombrearetematica=Area_Tematica.objects.get(id=area_tematica)
+
+        if area_tematica:
+            nombrearetematica = Area_Tematica.objects.get(id=area_tematica)
             print(nombrearetematica.cArea_tematica)
             lista_area_tematica = [nombrearetematica.cArea_tematica]
         else:
@@ -30,16 +31,14 @@ def generar_reporte_area(request):
         area_tematica = area_tematica if area_tematica else None
         print(f"Año: {año}, Área Temática: {area_tematica}")
 
-
         with connection.cursor() as cursor:
-            cursor.callproc('sp_report_proy_area', [area_tematica,año])
+            cursor.callproc('sp_report_proy_area', [area_tematica, año])
             results = cursor.fetchall()
- 
-        
+
         datosparticipante = defaultdict(lambda: {'proyectos': []})
 
         for row in results:
-            participante_id = row[1]  
+            participante_id = row[1]
             if participante_id not in datosparticipante:
 
                 datosparticipante[participante_id].update({
@@ -50,7 +49,7 @@ def generar_reporte_area(request):
                     'FUENTE_FINANCIAMIENTO': row[9],
                     'proyectos': []
                 })
-            
+
             fecha_inicio = row[5].strftime('%d/%m/%Y') if row[5] else ''
             fecha_fin = row[6].strftime('%d/%m/%Y') if row[6] else ''
 
@@ -73,9 +72,11 @@ def generar_reporte_area(request):
             }
             for participante_id, data in datosparticipante.items()
         ]
-        
-        
 
-        html_string = render(request, 'reportes/reporteproyectos-pdf.html', {'datosparticipante': datosparticipante, 'lista_area_tematica':lista_area_tematica})
+        fecha_actual = datetime.now().strftime('%d/%m/%Y')
+        hora_actual = datetime.now().strftime('%H:%M')
+
+        html_string = render(request, 'reportes/reporteproyectos-pdf.html', {
+                             'datosparticipante': datosparticipante, 'lista_area_tematica': lista_area_tematica, 'fecha_actual': fecha_actual, 'hora_actual': hora_actual})
 
         return JsonResponse({'html': html_string.content.decode('utf-8')})
